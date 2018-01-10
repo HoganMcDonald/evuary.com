4371ad656d581c5c1f64a963e545dc575ab40248
Hogan McDonald <hpeter.mcdonald@icloud.com>
2018-01-09 21:40:30 -0600

    boilerplate setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// routes
+app.route('/')
+  .get((req, res)=> {
+    res.sendFile(path.resolve('public/views/index.min.html'));
+  });
+
+// server
+const server = app.listen(process.env.PORT || 3000);
+module.exports = server;
