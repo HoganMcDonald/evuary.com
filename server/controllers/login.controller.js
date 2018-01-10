7333d7db5b0b2016d421ba669a204b9114f354de
Hogan McDonald <hpeter.mcdonald@icloud.com>
2018-01-10 13:25:53 -0600

    able to store user data

--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -1,3 +1,13 @@
 const passport = require('../models/facebookStrategy');
 
 module.exports.authenticate = passport.authenticate('facebook');
+
+module.exports.return = passport.authenticate('facebook', { failureRedirect: '/login' });
+
+module.exports.user = (req, res)=> {
+  res.json(req.user);
+}
+
+module.exports.redirect = (req, res)=> {
+  res.redirect('/');
+};
--- server/routes.js
+++ server/routes.js
@@ -5,4 +5,10 @@ const login = require('./controllers/login.controller');
 router.route('/login')
   .get(login.authenticate);
 
+router.route('/login/return')
+  .get(login.return, login.redirect);
+
+router.route('/profile')
+  .get(login.user)
+
 module.exports = router;
